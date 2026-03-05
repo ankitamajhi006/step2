2f3aeea6aa385e4c629732b624e2273808066cb6
ankitamajhi006 <ankitamajhi551@gmail.com>
2026-03-05 15:27:16 +0530

    c1

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,34 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-  //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-  // to see how IntelliJ IDEA suggests fixing it.
-  IO.println(String.format("Hello and welcome!"));
+/**
+ * Book My Stay App - Use Case 1
+ *
+ * This class is the starting point of the Book My Stay Hotel Booking system.
+ * It welcomes the user, displays the application name and version, and
+ * gives a friendly introduction to the service.
+ *
+ * Key Concepts Covered:
+ * - Class and main() method as program entry point
+ * - Static keyword usage
+ * - Console output with System.out.println()
+ * - JavaDoc for clear documentation
+ *
+ * Author: Student
+ * Version: 1.0
+ */
+public class UseCase1HotelBookingApp {
 
-  for (int i = 1; i <= 5; i++) {
-    //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-    // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-    IO.println("i = " + i);
-  }
+    /**
+     * Main method: JVM starts execution from here.
+     *
+     * @param args command-line arguments (not used in this use case)
+     */
+    public static void main(String[] args) {
+        System.out.println("*****************************************");
+        System.out.println("  Welcome to Book My Stay!               ");
+        System.out.println("  Your hassle-free hotel booking app     ");
+        System.out.println("  Version: 1.0                           ");
+        System.out.println("*****************************************");
+        System.out.println();
+        System.out.println("We're thrilled to have you here. Let's make your hotel booking experience simple and smooth!");
+        System.out.println("Sit back, relax, and explore the best stays around you.");
+    }
 }
